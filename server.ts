6ce243b2ed9c89aeac9b8f60ee78603fb896ce5f
mauro leal <wolfhackd@gmail.com>
2026-01-04 23:05:14 -0300

    fix: prisma-env error

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
-import 'dotenv/config';
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`
+})
+
 import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
 import { authRoutes } from './src/modules/auth.routes.js';
 import jwt from '@fastify/jwt';
 import fastifyCookie from '@fastify/cookie';
+
 
 const app = fastify({logger: true})
 const PORT = Number(process.env.PORT) || 3000
@@ -15,20 +21,27 @@ app.register(fastifyCors, {
 })
 
 //Configuration
+app.register(fastifyCookie,
+  {
+    secret: process.env.COOKIE_SECRET!,
+  }
+)
+
 app.register(jwt,{
   secret: process.env.JWT_SECRET!,
   cookie: {
     cookieName: 'token',
-    signed: false,
+    signed: true,
   },
 })
-app.register(fastifyCookie)
 
 //Routes
 app.register(authRoutes, {prefix: '/auth'})
 
 app.listen({port: PORT, host: '0.0.0.0'}, () => {
   try{
+    console.log('NODE_ENV:', process.env.NODE_ENV)
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
     console.log(`Server running on http://localhost:${PORT}`)
   }catch(e){
     console.log(e)
