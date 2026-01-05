704ec8102086cbc0ef276a41384b653a7da1fb94
mauro leal <wolfhackd@gmail.com>
2026-01-05 08:24:05 -0300

    feat: getMe Route

--- src/modules/auth.routes.ts
+++ src/modules/auth.routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import { login } from "./login.js";
 import { createUser } from "./createUser.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { getMe } from "./getMe.js";
 
 export const authRoutes = async (app: FastifyInstance) =>{
   app.post('/login', login )
-  app.post('/register', createUser )
+  app.post('/register' , createUser )
+  app.get('/me',{preHandler: authMiddleware}, getMe )
+  // app.post('/logout', createUser )
 }
