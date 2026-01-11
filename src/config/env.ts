142183c5ee16e1475123f569a36710fd24613ffa
mauro leal <wolfhackd@gmail.com>
2026-01-10 23:33:44 -0300

    update: env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,6 +11,10 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string().min(1, 'JWT_SECRET is required'),
   COOKIE_SECRET: z.string().min(1, 'COOKIE_SECRET is required'),
+  PASSWORD_SALT: z.number().default(10),
+  PASSWORD_SECRET: z.string().min(10, 'PASSWORD_SECRET must be at least 10 characters long'),
+  JWT_EXPIRES_IN_MINUTES: z.number().default(60),
+  JWT_ALGORITHM: z.string().default('HS256'),
 })
 
 const _env = envSchema.safeParse(process.env)
